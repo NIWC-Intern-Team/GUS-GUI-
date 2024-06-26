181ffe85edb036b5c1f6be2a7a14e86b96664d7d
trhussain <trhussain@ucsd.edu>
2024-06-26 10:12:07 -0700

    Restructuring again Accomplished --------- N/A
    In Progress
    ---------
    N/A
    
    Errors
    ----------
    N/A
    
    Next
    ----------
    N/A

---
+++ test/PyQtDarkTheme/examples/qpalette/apply_dark_palette.py
@@ -0,0 +1,17 @@
+import sys
+
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+import qdarktheme
+
+app = QApplication(sys.argv)
+main_win = QMainWindow()
+push_button = QPushButton("PyQtDarkTheme!!")
+main_win.setCentralWidget(push_button)
+
+# Apply dark theme
+app.setPalette(qdarktheme.load_palette())
+
+main_win.show()
+
+app.exec()
