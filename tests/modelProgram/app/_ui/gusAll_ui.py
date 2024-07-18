1c1d9567eb0035f8cc290ab209b4f0079d62b1f5
NShin99 <noahshin99@gmail.com>
2024-07-18 09:10:15 -0700

    Created new file error_warning.py to handle error thresholds

--- tests/modelProgram/app/_ui/gusAll_ui.py
+++ tests/modelProgram/app/_ui/gusAll_ui.py
@@ -186,7 +186,7 @@ class outerClass:
             
             
         def send_data_to_gui(self):
-            # Access CSV data and update table data with new info
+            # Access CSV data and update table data
             for i in range(1, self.tab + 1):
                 self.reload_csv_data()
                 
@@ -254,38 +254,13 @@ class outerClass:
             self.timer.timeout.connect(self.thresholds)
             self.timer.start(500)
             
+        # Checks for error thresholds
         def thresholds(self):
-            # Error Thresholds
-            for i in range(1, self.tab + 1):
-                self.reload_csv_data()
-                
-                # Get data from CSV files
-                self.battery = self.csv_handler.get_battery(i)
-                self.lat, self.long = self.csv_handler.get_lat_lon(i)
-                self.speed = self.csv_handler.get_speed(i)
-                self.temperature = self.csv_handler.get_average_temp(i)
-                self.heading = self.csv_handler.get_heading(i)
-                
-                if self.battery < 20: # battery threshold
-                    self.error_counter += 1
-                    self.errors_text_edit.append(f'Gus {i}: Error - Battery low ({self.battery})')
-                    
-                if self.lat > 200 or self.long > 200: # position threshold
-                    self.error_counter += 1
-                    self.errors_text_edit.append(f'Gus {i}: Error - Position too far ({self.lat}, {self.long})')
-                    
-                if self.speed > 50: # speed threshold
-                    self.error_counter += 1
-                    self.errors_text_edit.append(f'Gus {i}: Error - Speed above 50 m/s ({self.speed})')
-                    
-                if self.temperature > 80: # temperature threshold
-                    self.error_counter += 1
-                    self.errors_text_edit.append(f'Gus {i}: Error - Temperature too high ({self.temperature})')
-                
-                self.tab_widget.setTabText(0, f'Errors({self.error_counter})') # Update Error tab title
-                        
+            from data.error_warning import Error
+            Error.thresholds(self)
+        
         def reload_csv_data(self):
-            self.csv_handler.load_dataframes()        
+            self.csv_handler.load_dataframes()
             
             
 class allUI:
